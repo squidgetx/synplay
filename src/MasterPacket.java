895d3fd5eff4cdb057c0711121761970ff85fd7f
Charlie Proctor <charlie@charlieproctor.com>
2016-05-02 13:25:39 -0400

    adding start MasterPacket

---
+++ src/MasterPacket.java
@@ -0,0 +1,41 @@
+import java.nio.ByteBuffer;
+
+public class MasterPacket {
+//    time at which to play the song
+    private long timeToPlay;
+
+//    the music to play
+    private byte[] music;
+
+    public long getTimeToPlay() {
+        return timeToPlay;
+    }
+
+    public byte[] getMusic() {
+        return music;
+    }
+
+    public MasterPacket(long timeToPlay, byte[] music) {
+        this.timeToPlay = timeToPlay;
+        this.music = music;
+    }
+
+    public byte[] pack(){
+        int length = Long.BYTES + music.length;
+        ByteBuffer buffer = ByteBuffer.allocate(length);
+        buffer.putLong(this.timeToPlay);
+        buffer.put(this.music);
+
+        return buffer.array();
+    }
+
+    public static MasterPacket unpack(byte[] packet){
+        ByteBuffer buffer = ByteBuffer.wrap(packet);
+
+        long timeToPlay = buffer.getLong();
+        byte[] music = new byte[buffer.remaining()];
+        buffer.get(music);
+
+        return new MasterPacket(timeToPlay,music);
+    }
+}
