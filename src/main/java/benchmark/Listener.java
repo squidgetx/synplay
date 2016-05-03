c4d31774b6256d43470a75f7e3f3494d2e7810d3
Charlie Proctor <charlie@charlieproctor.com>
2016-05-03 12:36:01 -0400

    adding listen method

--- src/main/java/benchmark/Listener.java
+++ src/main/java/benchmark/Listener.java
@@ -4,19 +4,42 @@ import javax.sound.sampled.*;
 
 public class Listener {
     public static void main(String args[]) throws Exception {
-
+        AudioFormat format = Shouter.getFormat();
+        TargetDataLine line = Listener.openLine(format);
+        Listener.listen(line);
     }
 
-    public static void openLine(AudioFormat format) throws LineUnavailableException {
+    public static TargetDataLine openLine(AudioFormat format) throws LineUnavailableException {
         TargetDataLine line;
         DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+
         if (!AudioSystem.isLineSupported(info)) {
-            // Handle the error ...
             throw new LineUnavailableException();
         }
 
         // Obtain and open the line.
         line = (TargetDataLine) AudioSystem.getLine(info);
         line.open(format);
+
+        return line;
+    }
+
+    public static void listen(TargetDataLine line){
+
+        // Begin audio capture.
+        line.start();
+
+        int numBytesRead;
+        byte[] data = new byte[line.getBufferSize() / 5];
+
+        while (true) {
+            // Read bytes from the TargetDataLine
+            numBytesRead =  line.read(data, 0, data.length);
+
+            // And print the bytes to stdout
+            for (int i = 0; i < numBytesRead; i++){
+                System.out.println(data[i]);
+            }
+        }
     }
 }
