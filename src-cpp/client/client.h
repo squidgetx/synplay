44dfcedf3760649e2d927ebb2b869b4f9b913357
squidgetx <sylvan.zheng@yale.edu>
2016-05-04 14:09:31 -0400

    add basic client

--- src-cpp/client/client.h
+++ src-cpp/client/client.h
@@ -1,15 +1,18 @@
 #ifndef _CLIENT_H_
 #define _CLIENT_H_
 
+#include <iostream>
+
 class Client {
   public:
-    Client(int p) : port(p) {}
+    Client(int p) : port(p) {};
+    void start();
 
   private:
     int port;
 
 
 
-}
+};
 
 #endif
