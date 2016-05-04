0291aeaa3a1f40d28d9547b03c76d315009690c8
squidgetx <sylvan.zheng@yale.edu>
2016-05-04 15:37:23 -0400

    mpacket class

---
+++ src-cpp/util/mpacket.cc
@@ -0,0 +1,32 @@
+#include "mpacket.h"
+
+MPacket::MPacket(int time, char* p) : timestamp(time), payload(p) {
+
+}
+
+char* MPacket::get_payload() {
+  return payload;
+}
+
+long MPacket::get_timestamp() {
+  return timestamp;
+}
+
+const char * MPacket::pack() {
+  std::stringbuf buf;
+  buf.sputn(reinterpret_cast<char*>(timestamp), 4);
+  buf.sputn(payload, PACKET_SIZE);
+  return buf.str().data();
+}
+
+MPacket * MPacket::unpack(char * p) {
+  std::stringbuf buf;
+  buf.str(p);
+  long timestamp;
+  char * payload = new char[PACKET_SIZE];
+  buf.sgetn(reinterpret_cast<char*>(&timestamp), 4);
+  buf.sgetn(payload, PACKET_SIZE);
+  return new MPacket(timestamp, payload);
+}
+
+
---
+++ src-cpp/util/mpacket.h
@@ -0,0 +1,24 @@
+#ifndef _MPACKET_H_
+#define _MPACKET_H_
+
+#define PACKET_SIZE 1024
+
+#include <iostream>
+#include <sstream>
+
+class MPacket {
+  public:
+    MPacket(int time, char* p);
+    char* get_payload();
+    long get_timestamp();
+    const char* pack();
+    static MPacket* unpack(char * p);
+
+  private:
+    char* payload;
+    long timestamp;
+
+};
+
+
+#endif
