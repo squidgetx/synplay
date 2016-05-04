a1032dfceee666fb367b340a76ca767a87e4b628
Charlie Proctor <charlie@charlieproctor.com>
2016-05-04 14:43:11 -0400

    add ip addr to main

--- src-cpp/master/main.cc
+++ src-cpp/master/main.cc
@@ -1,20 +1,24 @@
 #include <iostream>
+#include <string>
+
 #include "master.h"
 
 using namespace std;
 
-#define USAGE "./main PORT\n"
+#define USAGE "./main ADDR PORT\n"
 
 int main(int argc, char *argv[])
 {
   
-  if (argc < 2){
+  if (argc < 3){
     cerr << USAGE << endl;
+    return 1;
   }
  
-  int port = atoi(argv[1]);
+  string ip_addr = argv[1];
+  int port = atoi(argv[2]);
 
-  Master *master = new Master(port);
+  Master *master = new Master(ip_addr,port);
 
   return 0;
 }
