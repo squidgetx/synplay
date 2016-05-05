3b4e227a369886feeee7fdd14e9a5c0b664f7549
squidgetx <sylvan.zheng@yale.edu>
2016-05-05 15:19:43 -0400

    client prints things

--- src-cpp/client/client.cc
+++ src-cpp/client/client.cc
@@ -1,14 +1,27 @@
 #include "client.h"
 
-static int pacallback(const void *in, void* out, unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void *userData) {
+static int pacallback(const void *inputBuffer, void* outputBuffer,
+    unsigned long framesPerBuffer, 
+    const PaStreamCallbackTimeInfo* timeInfo, 
+    PaStreamCallbackFlags statusFlags, 
+    void *userData) {
 
-  char *audio_out = (char*) out;
+  char *audio_out = (char*) outputBuffer;
   RingBuffer<char> * play_buffer = (RingBuffer<char> *) userData;
   // copy as much from the byte buffer to the 
   // audio out
+  bool endl = false;
   for(int i = 0; i < framesPerBuffer; i++) {
     *audio_out = play_buffer->get();
+    if (*audio_out != 0) {
+      std::cout << *audio_out;
+      endl = true;
+    }
     audio_out++;
+  }
+
+  if (endl) {
+    std::cout << std::endl;
   }
 
   return paContinue;
@@ -22,6 +35,7 @@ void Client::receive() {
     {
       if (!ec && bytes_recvd > 0)
       {
+        std::cout << "got " << bytes_recvd << "bytes" << std::endl;
         MPacket * mpacket = MPacket::unpack(data, bytes_recvd);
         mpacket->print();
         // For now just put everything in the play buffer
@@ -41,17 +55,31 @@ void Client::start() {
 
   /* Set up Port Audio */
   PaError err = Pa_Initialize();
+  if (err != paNoError) goto error;
   PaStream *stream;
-
-  // dummy data for now
 
   /* No input, 2 stereo out */
   err = Pa_OpenDefaultStream(&stream, 0, 2, paFloat32, SAMPLE_RATE, 4, pacallback, &play_buffer);
+  if (err != paNoError) goto error;
 
   receive();
+ 
+  err = Pa_StartStream(stream);
+  if (err != paNoError) goto error;
 
-  // listen for a packet
-  // print its contents!
+
+  //Pa_Sleep(100000);
+
+  //Pa_StopStream(stream);
+  //Pa_CloseStream(stream);
+  //Pa_Terminate();
+  return;
+error:
+  Pa_Terminate();
+  fprintf( stderr, "An error occured while using the portaudio stream\n" );
+  fprintf( stderr, "Error number: %d\n", err );
+  fprintf( stderr, "Error message: %s\n", Pa_GetErrorText( err ) );
+  return;
 
 }
 
