1cf163354588288ae5d8af8a3e45de47241a9007
squidgetx <sylvan.zheng@yale.edu>
2016-05-05 14:47:59 -0400

    update util

--- src-cpp/util/mpacket.h
+++ src-cpp/util/mpacket.h
@@ -8,14 +8,17 @@
 
 class MPacket {
   public:
-    MPacket(int time, char* p);
+    MPacket(int time, char* p, int psize);
     char* get_payload();
+    int get_payload_size();
     long get_timestamp();
+    void print();
     const char* pack();
     static MPacket* unpack(char * p, std::size_t n);
 
   private:
     char* payload;
+    int payload_size;
     long timestamp;
 
 };
