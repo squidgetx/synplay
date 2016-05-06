0fdf9efcbb6ba50a7558dbdc8e993ca466ceb5e3
David Hatch <dhatch387@gmail.com>
2016-05-06 17:10:13 -0400

    Fix seg faults in mutable buffer.

--- src-cpp/util/mutable_byte_buffer.cc
+++ src-cpp/util/mutable_byte_buffer.cc
@@ -3,11 +3,11 @@
 #include <cstdint>
 #include <cstring>
 
-MutableByteBuffer::MutableByteBuffer(std::size_t initial_size) : ByteBuffer(NULL, initial_size), write(0) {
+MutableByteBuffer::MutableByteBuffer(std::size_t initial_size) : ByteBuffer(NULL, initial_size), write_(0) {
   data_ = new uint8_t[size_];
 }
 
-MutableByteBuffer::MutableByteBuffer(const MutableByteBuffer &other) : ByteBuffer(NULL, other.size_), write(0) {
+MutableByteBuffer::MutableByteBuffer(const MutableByteBuffer &other) : ByteBuffer(NULL, other.size_), write_(0) {
   read_ = other.read_;
   write_ = other.write_;
   data_ = new uint8_t[size_];
@@ -36,9 +36,7 @@ void MutableByteBuffer::write(const uint8_t *data, std::size_t nbytes) {
       increase_size();
     }
 
-    *(data_ + write_) = *(data_);
-    data_++;
-    write_++;
+    *(data_ + write_++) = *(data++);
   }
 }
 
