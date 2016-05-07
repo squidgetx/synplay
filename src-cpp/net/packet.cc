1ffde93600c42f9bbb08eaf44d5df6b33880fd6e
Charlie Proctor <charlie@charlieproctor.com>
2016-05-07 16:28:14 -0400

    size - 1 from superpacket

--- src-cpp/net/packet.cc
+++ src-cpp/net/packet.cc
@@ -16,9 +16,9 @@ Packet* Packet::unpack(uint8_t* buf, std::size_t size){
     uint8_t type = buf[0];
     switch (type) {
       case TIME:
-        return TPacket::unpack(buf + 1,size);
+        return TPacket::unpack(buf + 1,size - 1);
       case DATA:
-        return MPacket::unpack(buf + 1,size);
+        return MPacket::unpack(buf + 1,size - 1);
       default:
         std::cerr << "Data corruption detected: Invalid flag." << std::endl;
     }
