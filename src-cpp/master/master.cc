29357ad9864d74fab60673c456b3e576ccac6b29
Charlie Proctor <charlie@charlieproctor.com>
2016-05-07 02:56:35 -0400

    small change in master time

--- src-cpp/master/master.cc
+++ src-cpp/master/master.cc
@@ -14,6 +14,9 @@ using namespace asio::ip;
 
 void Master::send_timesync() {
   TPacket tp;
+  tp.from_sent = get_millisecond_time();
+
+  // send the packet to the client
   socket.async_send_to(
       asio::buffer(tp.pack()), remote_endpt,
       [this](error_code, size_t) {
@@ -28,10 +31,18 @@ void Master::receive_timesync_reply() {
       [this](error_code e, size_t bytes_recvd) {
         // calculate sum shit
 
-        mtime_t from_recv = get_millisecond_time();
+      // immediately grab the receipt time  
+      mtime_t from_recv = get_millisecond_time();
+        
+      // unpack the time packet
         TPacket * tp = TPacket::unpack(tp_buffer);
+        tp->from_recvd = from_recv;
+
+        // calculate the offset
         mtime_t offset = (tp->to_recvd - tp->from_sent) - (tp->to_sent - tp->from_recvd);
         tp->offset = offset;
+
+        // and send the reply
         socket.async_send_to(
             asio::buffer(tp->pack()), remote_endpt,
             [this](error_code, size_t) {
