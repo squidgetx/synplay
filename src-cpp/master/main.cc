12a957bc8f18325ba1267bbe17f48a45621d4b5c
Charlie Proctor <charlie@charlieproctor.com>
2016-05-07 18:26:15 -0400

    pass multiple ip / port args

--- src-cpp/master/main.cc
+++ src-cpp/master/main.cc
@@ -2,24 +2,31 @@
 #include <string>
 
 #include "master.h"
+#include "asio.hpp"
 
 using namespace std;
+using namespace asio::ip;
 
-#define USAGE "./main ADDR PORT\n"
+#define USAGE "./main FILENAME [ADDR PORT]+\n"
 
 int main(int argc, char *argv[])
 {
   
-  if (argc < 3){
+  if (argc < 4 || argc % 2 != 0){
     cerr << USAGE << endl;
     return 1;
   }
  
-  string ip_addr = argv[1];
-  uint16_t port = atoi(argv[2]);
-  string filename = argv[3];
+  string filename = argv[1];
+  vector<udp::endpoint> remote_endpts;
 
-  Master *master = new Master(ip_addr,port,filename);
+  for (i = 2; i < argc; i += 2) {
+    string ip_addr = argv[i];
+    uint16_t port = atoi(argv[i+1]);
+    remote_endpts.push_back(udp::endpoint(address::from_string(ip_addr),port));
+  }
+
+  Master *master = new Master(filename,remote_endpts);
   master->run();
 
   return 0;
