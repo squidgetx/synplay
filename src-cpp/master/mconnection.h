d82ff6f41d52806b85b0802459e6e091f3dc2fa3
squidgetx <sylvan.zheng@yale.edu>
2016-05-08 23:29:47 -0400

    Add mconnection struct

---
+++ src-cpp/master/mconnection.h
@@ -0,0 +1,19 @@
+#ifndef M_CONNECTION_H
+#define M_CONNECTION_H
+
+typedef enum {
+  NAKED = 0,
+  SENT_INITIAL_TIMESYNC = 1,
+  SENT_FINAL_TIMESYNC = 2,
+  SENDING_DATA = 3
+} MConnectionState;
+
+typedef struct mconnection {
+  asio::deadline_timer timer;
+  MConnection() {
+    timer = NULL;
+    state = NAKED;
+  };
+} MConnection;
+
+#endif
